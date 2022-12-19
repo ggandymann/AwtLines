29e14d87d5e9f43f02f21303dac909c691e8e7d9
ggandymann <83089003+ggandymann@users.noreply.github.com>
2022-12-19 15:35:13 +0000

    Add files via upload

---
+++ BasicHelloWorld.java
@@ -0,0 +1,10 @@
+package stuff;
+
+public class BasicHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello there world");
+	}
+
+}
